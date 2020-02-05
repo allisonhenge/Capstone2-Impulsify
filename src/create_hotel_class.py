fadab986ba6b62c5d000dd0654552749d1a29a1a
xxx <file@xxxs-MacBook-Pro.local>
2020-02-05 14:20:00 -0700

    created classes, updated scripts and notebooks

---
+++ src/create_hotel_class.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import pandas.io.sql as psql
+from pandas import ExcelWriter
+from pandas import ExcelFile
+import matplotlib
+import matplotlib.pyplot as plt
+import openpyxl
+
---
+++ src/create_impulsify_databases.py
@@ -0,0 +1,13 @@
+table_names = ['adjustment_reasons', 'archive_histories', 'audit_logs', 'available_monthly_inventory_reports', 'brands', 'cart_discounts', 'carts', 
+                'cashiers', 'categories', 'central_tz_products', 'credit_data', 'currencies', 'dashes', 'delayed_jobs', 'departments', 
+                'distributor_products', 'distributors', 'eastern_tz', 'eastern_tz_products', 'eleven_pm', 'employees', 'extra_cart_infos', 'flags', 
+                'gateways', 'inventory_adjustments', 'inventory_items', 'inventory_snapshots', 'itemizer_infos', 'line_items', 'localizations', 'logs',
+                'management_companies', 'management_company_users', 'midnight_pm', 'monthly_inventory_reports', 'monthly_inventory_snapshots', 
+                'mountain_tz_products', 'mpd', 'mz_temp', 'nine_pm', 'pacific_tz_products', 'parameters', 'partner_users', 'partners', 
+                'payment_records', 'pms_exchanges', 'product_uploads', 'products', 'properties', 'property_stores', 'property_templates', 
+                'property_users', 'pu_lines', 'quick_keys', 'receive_events', 'roles', 'schema_migrations', 'sessions', 'store_actions', 'store_events',
+                'store_localizations', 'store_pendings', 'store_product_histories', 'store_products', 'store_shifts', 'store_users', 'stores', 'ten_pm', 
+                'tmpfile', 'under_performing_products', 'under_performing_products_with_rank', 'users', 'users_roles', 'vendors']
+
+# for name in table_names:
+#     name = load_pd_dataframe(str(name))
